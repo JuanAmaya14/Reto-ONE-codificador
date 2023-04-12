a19068b166adc59d2100bf462e4150a4e92a8fec
Amayap14 <juan.amaya.p14@gmail.com>
2023-04-12 16:33:39 -0500

    Verificar completo. Codificar o Decodificar

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,57 @@
-function imprimir() {
+function Verificar(codi_O_deco) {
 
-    event.preventDefault();
-    var texto = document.querySelector("#texto").value;
-    console.log(texto);
+    const especiales = /[\á\é\í\ó\ú\Á\É\Í\Ó\Ú\`\~\!\@\#\$\%\^\&\*\(\)\_\-\=\+\{\}\[\]\'\"\|\<\>\,\.\/\?\;\:]/g;
+    const mayusculas = /[A-Z]/g;
+
+    var texto = document.querySelector("#texto").value.trim();
+
+    if (texto.match(especiales) || texto.match(mayusculas)) {
+
+        alert("Tu texto tiene letras mayusculas o tiene caracteres especiales");
+
+    } else if (texto == "") {
+
+        alert("Esta vacio");
+
+    } else {
+
+        if (codi_O_deco == "codificar") {
+
+            Codificar();
+
+        } else if (codi_O_deco == "decodificar") {
+
+            Decodificar();
+
+        }
+
+    }
 
 }
 
+function Codificar() {
 
-var button = document.querySelector("#codificar");
-button.onclick = imprimir;
+    console.log("Codificar");
+
+}
+
+function Decodificar() {
+
+    console.log("Decodificar");
+
+}
+
+const reglas = { "a": "ai", "e": "enter", "i": "imes", "o": "ober", "u": "ufat" };
+
+//var codi = document.querySelector("#codificar");
+//codi.onclick = Verificar;
+
+document.querySelector("#codificar").onclick = function (event) {
+    event.preventDefault();
+    Verificar("codificar");
+}
+
+document.querySelector("#decodificar").onclick = function (event) {
+    event.preventDefault();
+    Verificar("decodificar");
+}
